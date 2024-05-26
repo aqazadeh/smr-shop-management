966ec5a7ec8affe28c45512c0c7ed9eafbb560a2
Rovsen <rovsenaqazadeh@gmail.com>
2024-05-27 02:14:26 +0400

    swagger api responses added integration cart service

--- cart-service/src/main/java/smr/shop/cart/service/configuration/SecurityConfig.java
+++ cart-service/src/main/java/smr/shop/cart/service/configuration/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
